617e363da9fa0ffec24b9e82c41d735450ab8c22
bigriver <bigriver@bigrivers-MacBook-Pro.local>
2022-04-01 05:42:28 -0400

    before outside-inside world slider

--- src/App.js
+++ src/App.js
@@ -1,116 +1,257 @@
-//import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import {
+  FormControl,
+  Stack,
+  Slider,
+  Box,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Button,
+} from '@mui/material';
+import { useState, useEffect, useLayoutEffect } from 'react';
 
-const questions = [
+const questionsData = [
+  {
+    id: 0,
+    text: 'Are your clousest onces Ok?',
+    result: 0,
+  },
   {
     id: 1,
-    text: 'Are your clousest Ok?',
+    text: 'Do you do what you have to do?',
+    result: 0,
   },
   {
     id: 2,
-    text: 'Do you do what you have to do?',
+    text: 'Do you do what you can do?',
+    result: 0,
   },
   {
     id: 3,
-    text: 'Do you do what you can do?',
-  },
-  {
-    id: 4,
     text: 'Do you really do what you whant to do?',
+    result: 0,
   },
 ];
 
 function App() {
-  const [question, setQuestion] = useState(0);
-  const [happy1, setHappy1] = useState(0);
-  const [happy2, setHappy2] = useState(0);
-  const [happy3, setHappy3] = useState(0);
-
+  const [questions, setQuestions] = useState(questionsData);
+  const [question, setQuestion] = useState(-1);
   const [selectedOption, setSelectedOption] = useState(0);
+
   const handleOptionChange = (changeEvent) => {
     setSelectedOption(changeEvent.target.value);
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    question === 3 ? setQuestion(0) : setQuestion(question + 1);
+    switch (question) {
+      case 0:
+        setQuestions((prevState) => [
+          ...prevState,
+          (prevState[0].result = parseInt(selectedOption, 10)),
+        ]);
+        break;
+      case 1:
+        setQuestions((prevState) => [
+          ...prevState,
+          (prevState[1].result = parseInt(selectedOption, 10)),
+        ]);
+        break;
+      case 2:
+        setQuestions((prevState) => [
+          ...prevState,
+          (prevState[2].result = parseInt(selectedOption, 10)),
+        ]);
+        break;
+      case 3:
+        setQuestions((prevState) => [
+          ...prevState,
+          (prevState[3].result = parseInt(selectedOption, 10)),
+        ]);
+        break;
+    }
+
+    setSelectedOption(0);
+    setQuestion(question + 1);
   };
 
-  return (
+  const refreshQuiz = () => {
+    setQuestion(-1);
+  };
+
+  const result =
+    ((questions[0].result +
+      questions[1].result +
+      questions[2].result +
+      questions[3].result) /
+      4) *
+    100;
+
+  return question === 4 ? (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>You are {result} % happy</h1>
+      <Button variant="outlined" onClick={refreshQuiz}>
+        Try again
+      </Button>
+    </div>
+  ) : question === -1 ? (
     <div className="App">
       <header className="App-header">
-        <h1>Are you happy?</h1>
+        <h1>Are you happy at this moment?</h1>
       </header>
-      <form onSubmit={handleFormSubmit}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '30px',
+        }}
+      >
+        <h2>How your happiness deppends on?</h2>
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '20px',
+          }}
+        >
+          <div
+            style={{
+              width: '10%',
+              padding: 10,
+            }}
+          >
+            My clousest
+          </div>
+          <Slider
+            defaultValue={50}
+            aria-label="Default"
+            valueLabelDisplay="auto"
+            sx={{ width: '50%' }}
+          />
+          <div
+            style={{
+              width: '10%',
+              padding: 10,
+            }}
+          >
+            My inner world
+          </div>
+        </Box>
+        <Button variant="outlined" onClick={handleFormSubmit}>
+          Next question
+        </Button>
+      </Box>
+    </div>
+  ) : (
+    <QuestionForm
+      handleFormSubmit={handleFormSubmit}
+      questions={questions}
+      handleOptionChange={handleOptionChange}
+      question={question}
+      selectedOption={selectedOption}
+    />
+  );
+}
+
+function QuestionForm(props) {
+  const {
+    handleFormSubmit,
+    questions,
+    handleOptionChange,
+    question,
+    selectedOption,
+  } = props;
+
+  return (
+    <div className="">
+      <header className="App-header">
+        <h1>Are you happy at this moment?</h1>
+      </header>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '20px',
+        }}
+      >
         <h2>{questions[question].text}</h2>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="point"
+        <FormControl>
+          <FormLabel id="label">Choose the answer</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
               value="1"
+              control={<Radio size="large" />}
+              label="Yes"
               onChange={handleOptionChange}
+              checked={selectedOption === '1'}
             />
-            1
-          </label>
-        </div>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="point"
-              value="2"
+            <FormControlLabel
+              value="0"
+              control={<Radio size="large" />}
+              label="No"
               onChange={handleOptionChange}
+              checked={selectedOption === '0'}
             />
-            2
-          </label>
-        </div>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="point"
-              value="3"
-              onChange={handleOptionChange}
-            />
-            3
-          </label>
-        </div>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="point"
-              value="4"
-              onChange={handleOptionChange}
-            />
-            4
-          </label>
-        </div>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="point"
-              value="5"
-              onChange={handleOptionChange}
-            />
-            5
-          </label>
-        </div>
-        <div className="form-group">
-          <button className="" type="submit">
-            Submit
-          </button>
-        </div>
-        {selectedOption}
-        <p>happy1 = {happy1}</p>
-        <p>happy2 = {happy2}</p>
-        <p>happy3 = {happy3}</p>
-      </form>
+          </RadioGroup>
+        </FormControl>
+        <Button variant="outlined" onClick={handleFormSubmit}>
+          Next question
+        </Button>
+      </Box>
     </div>
   );
 }
 
 export default App;
+
+// <form onSubmit={handleFormSubmit}>
+// <h2>{questions[question].text}</h2>
+// <div className="form-check">
+//   <label>
+//     <input
+//       type="radio"
+//       name="point"
+//       checked={selectedOption === '0'}
+//       value="0"
+//       onChange={handleOptionChange}
+//     />
+//     No
+//   </label>
+// </div>
+// <div className="form-check">
+//   <label>
+//     <input
+//       type="radio"
+//       name="point"
+//       checked={selectedOption === '1'}
+//       value="1"
+//       onChange={handleOptionChange}
+//     />
+//     Yes
+//   </label>
+// </div>
+// <div className="form-group">
+//   <button className="" type="submit">
+//     Submit
+//   </button>
+// </div>
+// </form>
